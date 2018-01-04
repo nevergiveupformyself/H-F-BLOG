ce220c017957e18788b902329190ab633c04b6fa
11330 <627600810@qq.com>
2018-01-04 16:08:20 +0800

    登录修改 --老冯

--- src/main/java/com/hf/controllers/LoginController.java
+++ src/main/java/com/hf/controllers/LoginController.java
@@ -6,6 +6,7 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,17 +17,21 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login(String username,String password,HttpSession session) {
+    public ModelAndView login(String username, String password, HttpSession session) {
         UsernamePasswordToken usernamePasswordToken=new UsernamePasswordToken(username,password);
         //所有Subject都绑定到SecurityManager，与Subject的所有交互都会委托给SecurityManager；可以把Subject认为是一个门面；SecurityManager才是实际的执行者
         Subject subject = SecurityUtils.getSubject();
+        ModelAndView modelAndView;
         try {
             subject.login(usernamePasswordToken);   //完成登录
             User user=(User) subject.getPrincipal();
             session.setAttribute("user", user);
-            return "index";
+            modelAndView = new ModelAndView("/index");
+            return modelAndView;
         } catch(Exception e) {
-            return "login";//返回登录页面
+            modelAndView = new ModelAndView("/login");
+            modelAndView.addObject("message","账号或密码错误");
+            return modelAndView;//返回登录页面
         }
 
     }
