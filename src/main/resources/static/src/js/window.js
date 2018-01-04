32978e8fc0256d68211bd8eb76f77fe8e8267bb5
10991 <xuhailin123>
2018-01-04 12:45:32 +0800

    [ADD] logo --H

---
+++ src/main/resources/static/src/js/window.js
@@ -0,0 +1,19 @@
+/*
+dialog
+ */
+(function($,hf){
+    "use strict";
+
+    var Plugin = hf.Plugin('window',{
+        defults:{
+            title:"",
+            name:"Window",
+            action:['close'],
+            autoFocus:true,
+            modal:false,
+            appendTo:"body",
+            width:null,
+            height:null
+        }
+    })
+}(jQuery))
