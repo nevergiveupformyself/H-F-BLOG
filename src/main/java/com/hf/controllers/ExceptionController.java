ee8ec1af7b99fe96ac373b182e403114b5a66f00
11330 <627600810@qq.com>
2018-01-16 17:45:47 +0800

    json注解 --老冯

--- src/main/java/com/hf/controllers/ExceptionController.java
+++ src/main/java/com/hf/controllers/ExceptionController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -35,6 +36,7 @@ public class ExceptionController {
 //    }
 
     @ExceptionHandler(value = { Exception.class })
+    @ResponseBody
     public Object exceptionHandler(Exception exception, HttpServletRequest request) {
         String message = exception.getMessage();
         log.error(message, exception);
